8bcff91b7110014cc0496c68ee81c815209e41fc
ahenrick <share.code@aol.com>
2019-03-05 06:45:00 -0800

    updates

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Compile Proto
+`protoc -I=. --js_out=import_style=commonjs:src/app/ --grpc-web_out=import_style=typescript,mode=grpcwebtext:src/app/ calculator.proto`
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as grpcWeb from 'grpc-web';
+import {NumberRequest, NumberResponse} from './calculator_pb';
+import {CalculatorClient} from './CalculatorServiceClientPb';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-grpc-web-client';
+  calculatedValue = 0 ;
+
+  squareRoot(value) {
+    console.log(value);
+    const calculatorClient = new CalculatorClient('http://localhost:8080', null, null);
+
+    const request = new NumberRequest();
+    request.setValue(value);
+
+    const call = calculatorClient.squareRoot(request, {'custom-header-1': 'value1'},
+      (err: grpcWeb.Error, response: NumberResponse) => {
+        this.calculatedValue = response.getValue()
+        console.log(response.getValue());
+      });
+    call.on('status', (status: grpcWeb.Status) => {
+      // ...
+    });
+
+  }
 }
